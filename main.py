2d68e10e36745ecc061bf0d5c78c5a67418ae3b3
Jignesh Jakkula <jakkulajignesh95@gmail.com>
2024-07-29 10:02:38 +0530

    second commit

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import pyttsx3
 import pywhatkit
 import datetime
 import wikipedia
+import webbrowser
+
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
+
+
 
 def take_command():
     try:
@@ -24,6 +29,7 @@ def take_command():
                 command = command.replace('alexa', '')
                 print(command)
             
+    
     except:
         pass
     return command
@@ -32,26 +38,72 @@ def take_command():
 def run_alexa():
     command = take_command()
     print(command)
-    if 'play' in command:
+    if 'play' in command: #youtube cmd
         song = command.replace('play', '')
         talk('playing' + song)
         pywhatkit.playonyt(song)
+    
+# time cmd    
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I %M %p')
         print(time)
         talk("it's" + time)
+
+#date cmd   
+    elif 'date' in command:
+        date = datetime.datetime.now().strftime('%A %B %d, %Y')
+        print(date)
+        talk(date)
     
+    
+#web commands    
     elif 'tell me about' in command:
         person = command.replace('tell me about', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
-    else:
-        talk('ydheyyy')
+    
+    
+    elif 'open' in command:
+        url = command.replace('open', '')
+        webbrowser.open(url)
+    
+    
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    
+    
+    elif 'search' in command:
+        search = command.replace('search', '')
+        url = 'https://www.google.com/search?q=' + search
+        webbrowser.open(url)
+    
+    
+    elif 'what is' in command:
+        person = command.replace('what is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    
+    
+    elif 'how to' in command:
+        how = command.replace('how to', '')
+        url = 'https://www.youtube.com/results?search_query=' + how
+        webbrowser.open(url)
+    
+    
+    elif 'stop' in command:
+        print('stopping the program')
+        talk("stopping the program")
+        return False
 
-
+        
 while True:
-    run_alexa()
+    if not run_alexa():
+        break
 
 
     
